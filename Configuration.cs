5f86396926ad57abc212eee150a1e5c0b3d8f2c7
Jack Waterfall <jack.a.waterfall@gmail.com>
2023-11-24 23:04:26 +0000

    feat: basic player controller

--- Configuration.cs
+++ Configuration.cs
@@ -3,6 +3,10 @@ using System;
 
 public partial class Configuration : Node
 {
-	public static Vector3 CHUNK_DIMENSION = new(16, 32, 16);
-	public static Vector2 TEXTURE_ATLAS_SIZE = new(8, 2);
+	public static readonly Vector3 CHUNK_DIMENSION = new(16, 32, 16);
+	public static readonly Vector2 TEXTURE_ATLAS_SIZE = new(8, 2);
+	public static readonly float MOUSE_SENSITIVITY = 0.3f;
+	public static readonly float MOVEMENT_SPEED = 10;
+	public static readonly float GRAVITY = 20;
+	public static readonly float JUMP_VELOCITY = 10;
 }
---
+++ Player.cs
@@ -0,0 +1,89 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	private bool Paused = false;
+	private float CameraXRotation = 0;
+
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+
+	public override void _Input(InputEvent @event)
+    {
+		Node3D Head = GetNode<Node3D>("Head");
+		Camera3D Camera = GetNode<Camera3D>("Head/Camera3D");
+
+        if (@event.IsActionPressed("Pause"))
+        {
+			Paused = !Paused;
+			if (Paused)
+            {
+                Input.MouseMode = Input.MouseModeEnum.Visible;
+            }
+			else
+			{
+                Input.MouseMode = Input.MouseModeEnum.Captured;
+            }
+        }
+
+		if (Paused)
+		{
+			return;
+		}
+
+		if (@event is InputEventMouseMotion)
+		{
+			Head.RotateY(Mathf.DegToRad(-(@event as InputEventMouseMotion).Relative.X * Configuration.MOUSE_SENSITIVITY));
+
+			float DeltaX = (@event as InputEventMouseMotion).Relative.Y * Configuration.MOUSE_SENSITIVITY;
+			if (CameraXRotation + DeltaX > -90 && CameraXRotation + DeltaX < 90)
+            {
+				Camera.RotateX(Mathf.DegToRad(-DeltaX));
+				CameraXRotation += DeltaX;
+			}
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+		Vector3 NewVelocity = Velocity;
+        Node3D Head = GetNode<Node3D>("Head");
+
+        if (Paused)
+		{
+			return;
+		}
+
+		if (!IsOnFloor())
+		{
+            NewVelocity.Y -= Configuration.GRAVITY * (float)delta;
+		}
+
+		if (Input.IsActionJustPressed("Jump"))
+		{
+            NewVelocity.Y = Configuration.JUMP_VELOCITY;
+        }
+
+        Basis Basis = Head.GlobalTransform.Basis;
+		Vector2 InputDirection = Input.GetVector("Left", "Right", "Forward", "Backward");
+		Vector3 Direction = (Basis * new Vector3(InputDirection.X, 0, InputDirection.Y)).Normalized();
+
+		if (Direction.Length() != 0)
+		{
+            NewVelocity.X = Direction.X * Configuration.MOVEMENT_SPEED;
+            NewVelocity.Z = Direction.Z * Configuration.MOVEMENT_SPEED;
+        }
+		else
+		{
+            NewVelocity.X = Mathf.MoveToward(Velocity.X, 0, Configuration.MOVEMENT_SPEED);
+            NewVelocity.Z = Mathf.MoveToward(Velocity.Z, 0, Configuration.MOVEMENT_SPEED);
+        }
+
+		Velocity = NewVelocity;
+
+		MoveAndSlide();
+    }
+}
