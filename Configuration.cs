2869095f8f0b0365ad53e892d1e2ca90bd7cb148
Jack Waterfall <jack.a.waterfall@gmail.com>
2023-11-25 15:54:03 +0000

    fix: player no longer gets stuck on corner of blocks and tweaked jump velocity and movement speed

--- Configuration.cs
+++ Configuration.cs
@@ -6,9 +6,9 @@ public partial class Configuration : Node
 	public static readonly Vector3I CHUNK_DIMENSION = new(32, 32, 32);
 	public static readonly Vector2I TEXTURE_ATLAS_SIZE = new(8, 2);
 	public static readonly float MOUSE_SENSITIVITY = 0.3f;
-	public static readonly float MOVEMENT_SPEED = 10;
+	public static readonly float MOVEMENT_SPEED = 5;
 	public static readonly float GRAVITY = 20;
-	public static readonly float JUMP_VELOCITY = 10;
+	public static readonly float JUMP_VELOCITY = 6.25f;
 	public static readonly int CHUNK_LOAD_RADIUS = 2;
 	public static readonly float CHUNK_UNLOAD_TIME = 10;
 	public static readonly bool FLYING_ENABLED = false;
