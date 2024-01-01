46223424ac21e5502283802a3ad13badf52ddb7b
Jack Waterfall <jack.a.waterfall@gmail.com>
2024-01-01 14:58:16 +0000

    chore: scene pascal casing and increased jump velocity

--- Configuration.cs
+++ Configuration.cs
@@ -9,7 +9,7 @@ namespace Aeon
         public static readonly float MOUSE_SENSITIVITY = 0.3f;
         public static readonly float MOVEMENT_SPEED = 50;
         public static readonly float GRAVITY = 30;
-        public static readonly float JUMP_VELOCITY = 7.5f;
+        public static readonly float JUMP_VELOCITY = 8f;
         public static readonly bool FLYING_ENABLED = true;
         public static readonly int CHUNK_LOAD_RADIUS = 6;
         public static readonly int MAX_STALE_CHUNKS = 100;
