31cab8450b446fe2c84ae3888ef47b1ecb5cfaa7
Jack Waterfall <jack.a.waterfall@gmail.com>
2024-06-08 11:57:16 +0100

    feat: further chunk data optimization

--- world/ChunkData.cs
+++ world/ChunkData.cs
@@ -22,111 +22,152 @@ namespace Aeon
         public abstract void Optimize(Chunk chunk);
     }
 
-    class StandardChunkData : ChunkData
+    abstract class ChunkLayerData
+    {
+        protected readonly Vector2I _dimensions;
+        protected readonly int _layerIndex;
+
+        protected ChunkLayerData(Vector2I dimensions, int layerIndex)
+        {
+            _dimensions = dimensions;
+            _layerIndex = layerIndex;
+        }
+
+        protected int GetIndex(Vector2I localPosition)
+        {
+            return (localPosition.Y * _dimensions.X) + localPosition.X;
+        }
+
+        public abstract BlockType GetBlock(Vector2I localPosition);
+        public abstract void SetBlock(StandardChunkData chunkData, Vector2I localPosition, BlockType blockType);
+        public abstract void Optimize(StandardChunkData chunkData);
+    }
+
+
+    class StandardChunkLayerData : ChunkLayerData
     {
         private byte[] _blocks;
 
-        public StandardChunkData(Vector3I dimensions, byte[] blocks) : base(dimensions)
+        public StandardChunkLayerData(Vector2I dimensions, byte[] blocks, int layerIndex) : base(dimensions, layerIndex)
         {
             _blocks = blocks;
         }
 
-        public StandardChunkData(Vector3I dimensions) : base(dimensions)
+        public StandardChunkLayerData(Vector2I dimensions, int layerIndex) : base(dimensions, layerIndex)
         {
-            _blocks = new byte[dimensions.X * dimensions.Z * dimensions.Y];
+            _blocks = new byte[dimensions.X * dimensions.Y];
         }
 
-        public override BlockType GetBlock(Vector3I localPosition)
+        public override BlockType GetBlock(Vector2I localPosition)
         {
             return BlockTypes.Instance.Get(_blocks[GetIndex(localPosition)]);
         }
 
-        public override void SetBlock(Chunk chunk, Vector3I localPosition, BlockType blockType)
+        public override void SetBlock(StandardChunkData chunkData, Vector2I localPosition, BlockType blockType)
         {
             _blocks[GetIndex(localPosition)] = blockType.Id;
         }
 
-        public override void Optimize(Chunk chunk)
+        public override void Optimize(StandardChunkData chunkData)
         {
-            var isSingleBlock = Array.TrueForAll(_blocks, blockId => blockId == _blocks[0]);
+            //var isSingleBlock = Array.TrueForAll(_blocks, blockId => blockId == _blocks[0]);
 
-            if (isSingleBlock)
-            {
-                var block = BlockTypes.Instance.Get(_blocks[0]);
-                chunk.SetChunkData(new SingleBlockChunkData(_dimensions, block));
-            }
-            else
-            {
-                bool canOptimize = true;
-                byte[] layerBlockIds = new byte[_dimensions.Y];
-                var blocksInLayer = _dimensions.X * _dimensions.Z;
-
-                for (int y = 0; y < _dimensions.Y; y++)
-                {
-                    var layer = new byte[blocksInLayer];
-                    Array.Copy(_blocks, y * blocksInLayer, layer, 0, blocksInLayer);
-
-                    var firstBlockId = layer[0];
-                    var isSameBlock = Array.TrueForAll(layer, blockId => blockId == firstBlockId);
-
-                    if (isSameBlock)
-                    {
-                        layerBlockIds[y] = firstBlockId;
-                    }
-                    else
-                    {
-                        canOptimize = false;
-                        break;
-                    }
-                }
-
-                if (canOptimize)
-                {
-                    var blocks = Array.ConvertAll(layerBlockIds, blockId => BlockTypes.Instance.Get(blockId));
-                    chunk.SetChunkData(new VerticalLayerChunkData(_dimensions, blocks));
-                }
-            }
+            //if (isSingleBlock)
+            //{
+            //    var block = BlockTypes.Instance.Get(_blocks[0]);
+            //    chunkData.SetLayerData(_dimensions.Y, new SingleBlockChunkLayerData(_dimensions, block, _layerIndex));
+            //}
         }
     }
 
-    class VerticalLayerChunkData : ChunkData
+
+    class SingleBlockChunkLayerData : ChunkLayerData
     {
-        private BlockType[] _layerBlocks;
+        private BlockType _block;
 
-        public VerticalLayerChunkData(Vector3I dimensions, BlockType[] layerBlocks) : base(dimensions)
+        public SingleBlockChunkLayerData(Vector2I dimensions, BlockType block, int layerIndex) : base(dimensions, layerIndex)
         {
-            this._layerBlocks = layerBlocks;
+            _block = block;
         }
 
-        public override BlockType GetBlock(Vector3I localPosition)
+        public override BlockType GetBlock(Vector2I localPosition)
         {
-            return _layerBlocks[localPosition.Y];
+            return _block;
         }
 
-        public override void SetBlock(Chunk chunk, Vector3I localPosition, BlockType blockType)
+        public override void SetBlock(StandardChunkData chunkData, Vector2I localPosition, BlockType blockType)
         {
-            var blocks = new byte[_dimensions.X * _dimensions.Z * _dimensions.Y];
+            var blocks = new byte[_dimensions.X * _dimensions.Y];
 
-            for (int y = 0; y < _dimensions.Y; y++)
+            for (int i = 0; i < blocks.Length; i++)
             {
-                var blockId = blockType.Id;
-                for (int x = 0; x < _dimensions.X; x++)
-                {
-                    for (int z = 0; z < _dimensions.Z; z++)
-                    {
-                        blocks[y * _dimensions.Z * _dimensions.X + z * _dimensions.X + x] = blockId;
-                    }
-                }
+                blocks[i] = blockType.Id;
             }
 
             blocks[GetIndex(localPosition)] = blockType.Id;
 
-            chunk.SetChunkData(new StandardChunkData(_dimensions, blocks));
+            chunkData.SetLayerData(_layerIndex, new StandardChunkLayerData(_dimensions, blocks, _layerIndex));
+        }
+
+        public override void Optimize(StandardChunkData chunkData)
+        {
+            // Can't optimize further
+        }
+    }
+
+
+    class StandardChunkData : ChunkData
+    {
+        private ChunkLayerData[] _layers;
+
+        public StandardChunkData(Vector3I dimensions, ChunkLayerData[] layers) : base(dimensions)
+        {
+            _layers = layers;
+        }
+
+        public StandardChunkData(Vector3I dimensions) : base(dimensions)
+        {
+            _layers = new ChunkLayerData[dimensions.Y];
+
+            for (int y = 0; y < dimensions.Y; y++)
+            {
+                _layers[y] = new StandardChunkLayerData(new Vector2I(dimensions.X, dimensions.Z), y);
+            }
+        }
+
+        public override BlockType GetBlock(Vector3I localPosition)
+        {
+            return _layers[localPosition.Y].GetBlock(new Vector2I(localPosition.X, localPosition.Z));
+        }
+
+        public override void SetBlock(Chunk chunk, Vector3I localPosition, BlockType blockType)
+        {
+            _layers[localPosition.Y].SetBlock(this, new Vector2I(localPosition.X, localPosition.Z), blockType);
+        }
+
+        public void SetLayerData(int layerIndex, ChunkLayerData layerData)
+        {
+            _layers[layerIndex] = layerData;
         }
 
         public override void Optimize(Chunk chunk)
         {
-            // No point in optimizing from here as players can only place a single block at a time
+            var areAllSingleBlockLayers = Array.TrueForAll(_layers, layer => layer is SingleBlockChunkLayerData);
+            var firstLayerBlock = (_layers[0]).GetBlock(new Vector2I(0, 0));
+            var isSingleBlock = areAllSingleBlockLayers &&
+                Array.TrueForAll(_layers, layer => ((SingleBlockChunkLayerData)layer).GetBlock(new Vector2I(0, 0)) == firstLayerBlock);
+
+            if (isSingleBlock)
+            {
+                chunk.SetChunkData(new SingleBlockChunkData(_dimensions, firstLayerBlock));
+            }
+            else
+            {
+                foreach (var layer in _layers)
+                {
+                    layer.Optimize(this);
+                }
+            }
         }
     }
 
@@ -146,16 +187,25 @@ namespace Aeon
 
         public override void SetBlock(Chunk chunk, Vector3I localPosition, BlockType blockType)
         {
-            var blocks = new byte[_dimensions.X * _dimensions.Z * _dimensions.Y];
+            var chunkData = new StandardChunkData(_dimensions);
+            var layers = new ChunkLayerData[_dimensions.Y];
 
-            for (int i = 0; i < blocks.Length; i++)
+            for (int y = 0; y < _dimensions.Y; y++)
             {
-                blocks[i] = blockType.Id;
+                layers[y] = new SingleBlockChunkLayerData(new Vector2I(_dimensions.X, _dimensions.Z), blockType, y);
             }
 
-            blocks[GetIndex(localPosition)] = blockType.Id;
+            var modifiedLayerBlocks = new byte[_dimensions.X * _dimensions.Z];
 
-            chunk.SetChunkData(new StandardChunkData(_dimensions, blocks));
+            for (int i = 0; i < modifiedLayerBlocks.Length; i++)
+            {
+                modifiedLayerBlocks[i] = blockType.Id;
+            }
+
+            modifiedLayerBlocks[(localPosition.Z * _dimensions.X) + localPosition.X] = blockType.Id;
+            layers[localPosition.Y] = new StandardChunkLayerData(new Vector2I(_dimensions.X, _dimensions.Z), modifiedLayerBlocks, localPosition.Y);
+
+            chunkData.SetLayerData(localPosition.Y, layers[localPosition.Y]);
         }
 
         public override void Optimize(Chunk chunk)
